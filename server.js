868a5c9c5452b5f455266f28784e376441f1eca4
Addison Philips <addyphilips66@gmail.com>
2023-02-24 23:29:46 +0000

    finished

--- server.js
+++ server.js
@@ -7,17 +7,24 @@ const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
 
-app.get('/restaurants', async(req, res) => {
-    try {
-        const restaurants = await Restaurant.findAll();
-        res.json(restaurants);
-    } catch (err) {
-        console.log(err)
-        res.status(500).send("An error occured while retrieving restaurants")
-        
-    }
+app.get("/restaurants", async (req, res) => {
+  try {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occured while retrieving restaurants");
+  }
+});
 
-})
+app.get("/restaurants/:id", async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.id);
+    res.send(restaurant);
+  } catch (err) {
+    res.status(404).send("Restaurant not found");
+  }
+});
 
 
 app.listen(port, () => {
