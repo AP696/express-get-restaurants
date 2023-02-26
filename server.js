2db26bb15782e1fb6d9677ef09b693981976b933
Addison Philips <addyphilips66@gmail.com>
2023-02-26 19:11:26 +0000

    1

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const {sequelize} = require("./db");
 
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
+
+app.use(express.json());
 
 app.get("/restaurants", async (req, res) => {
   try {
@@ -25,6 +27,59 @@ app.get("/restaurants/:id", async (req, res) => {
     res.status(404).send("Restaurant not found");
   }
 });
+
+app.post("/restaurants", async (req, res) => {
+  try {
+    const { name, description, rating } = req.body;
+    const restaurant = await Restaurant.create({
+      name,
+      description,
+      rating
+    });
+    res.status(201).json(restaurant);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occured while creating the restaurant");
+  }
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  try {
+    const { name, description, rating } = req.body;
+    const updatedRestaurant = await Restaurant.update({
+      name, 
+      description,
+      rating
+    },
+    {
+      where: { id: req.params.id }
+    });
+    if(updatedRestaurant[0]) {
+      res.status(200).send("Restaurant updated successfully");
+    } else {
+      res.status(404).send("Restaurant not found")
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occured while retrieving restaurants");
+  }
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  try {
+    const deletedRestaurant = await Restaurant.destroy({
+      where: { id: req.params.id }
+    });
+    if (deletedRestaurant) {
+      res.status(200).send("Restaurant deleted successfully");
+    } else {
+      res.status(404).send("Restaurant not found");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occured while deleting restaurant");
+  }
+})
 
 
 app.listen(port, () => {
